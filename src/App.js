6f10649f18cf062645c9a30ebb220bcdecf3e0c4
Marcus Santos <marcusdcarli@gmail.com>
2019-10-23 06:53:48 -0300

    Aula 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import Tabela from './Tabela';
 
 function App() {
+  const autores = [
+    {
+      nome: 'Paulo',
+      livro: 'React',
+      preco: '1000'
+    },
+    {
+      nome: 'Daniel',
+      livro: 'Java',
+      preco: '99'
+    },
+    {
+      nome: 'Marcos',
+      livro: 'Design',
+      preco: '150'
+    },
+    {
+      nome: 'Bruno',
+      livro: 'DevOps',
+      preco: '100'
+    }
+  ];
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Tabela autores={autores} />
     </div>
   );
 }
